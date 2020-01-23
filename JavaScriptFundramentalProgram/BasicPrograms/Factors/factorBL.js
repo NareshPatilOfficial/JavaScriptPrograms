2ebff1a5ddcc6333747679018f317bffcf94b73a
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-01-23 23:23:16 +0530

    [Naresh] Merge Branch to Master

--- JavaScriptFundramentalProgram/BasicPrograms/Factors/factorBL.js
+++ JavaScriptFundramentalProgram/BasicPrograms/Factors/factorBL.js
@@ -3,31 +3,24 @@
  * FuctionalPrograms/Factors.
  * @module BasicPrograms/Factors
  */
-
 module.exports = {
-
     /**
-     * Genarate .
+     * Genarate Factores.
      * @param {number} n - it take number for find prime factores.
      * @return {number} factor .
      */
-    factorOf:function(n){
-        while (n % 2 == 0)  
-            {  
-                n = n/2;  
+    factorOf: (n) => {
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        for (let i = 3; i <= n * n; i = i + 2) {
+            // While i divides n, print i and divide n  
+            while (n % i == 0) {
+                console.log(i);
+                n = n / i;
             }
-            
-            for (let i = 3; i <= n*n; i = i + 2)  
-            {  
-                // While i divides n, print i and divide n  
-                while (n % i == 0)  
-                {  
-                    console.log(i);
-                    n = n/i;  
-                }  
-            }  
-
-            if (n > 2)  
-                console.log(n);
-            }
+        }
+        if (n > 2)
+            console.log(n);
+    }
 }
--- JavaScriptFundramentalProgram/BasicPrograms/Factors/factorsMain.js
+++ JavaScriptFundramentalProgram/BasicPrograms/Factors/factorsMain.js
@@ -4,7 +4,6 @@
  *  Purpose         : Get Prime Factors.
  *
  *  @description    : Computes the prime factorization of N using brute force.
- *
  *  @file           : factorMain.js
  *  @overview       : Genrate Prime Factorial Of given number 
  *  @module         : module_name - This is optional if expeclictly its an npm or local package
@@ -12,15 +11,11 @@
  *  @version        : 1.0
  *  @since          : 20-01-2020
  ******************************************************************************/
-
-
-const readLine = require('F:/BridgeLabzAngular/Angular/JavaScriptFundramentalProgram/node_modules/readline-sync');
+const readLine = require('readline-sync');
 const factorBL = require('./factorBL');
-
-try{
-    let n = 315;
-    let num = readLine.question('Enter The Number : ');
+try {
+    let num = readLine.questionInt('Enter The Number : ');
     factorBL.factorOf(num);
-}catch(err){
+} catch (err) {
     console.log(err);
 }
