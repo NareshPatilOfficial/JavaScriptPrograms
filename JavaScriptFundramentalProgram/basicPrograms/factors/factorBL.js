d576be0d0bc6db8d4910ae8df93e4136d2bdc6fe
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-01-24 00:07:19 +0530

    [Naresh] refactor using Arrow function in factorBL.js

---
+++ JavaScriptFundramentalProgram/basicPrograms/factors/factorBL.js
@@ -0,0 +1,26 @@
+
+/**
+ * FuctionalPrograms/Factors.
+ * @module BasicPrograms/Factors
+ */
+module.exports = {
+    /**
+     * Genarate Factores.
+     * @param {number} n - it take number for find prime factores.
+     * @return {number} factor .
+     */
+    factorOf: (n) => {
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        for (let i = 3; i <= n * n; i = i + 2) {
+            // While i divides n, print i and divide n  
+            while (n % i == 0) {
+                console.log(i);
+                n = n / i;
+            }
+        }
+        if (n > 2)
+            console.log(n);
+    }
+}
