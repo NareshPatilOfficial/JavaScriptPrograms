52f773aa724f8f5b8d9863283d42124088d2c12a
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-01-24 09:56:14 +0530

    [Naresh] refactore use arrow function in replaceUserNameBL.js

---
+++ JavaScriptFundramentalProgram/basicPrograms/validName/replaceUserNameBL.js
@@ -0,0 +1,15 @@
+/**
+ * FuctionalPrograms/replaceUserNameBL.
+ * @module BasicPrograms/replaceUserNameBL
+ */
+module.exports = {
+    /**
+     * covert into valide Username.
+     * @param {string} userName - it take username as string.
+     * @return {string} valid username.
+     */
+    validName: (userName) => {
+        var result = userName.replace(/[^a-zA-Z ]/g, "");
+        return result;
+    }
+};
