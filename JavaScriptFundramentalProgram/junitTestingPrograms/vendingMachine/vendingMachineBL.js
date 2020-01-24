3cf955311946debfe55134c04efeb7a6e9d3b84c
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-01-24 11:42:44 +0530

    [Naresh] replace old syntax to ES6 fetured syntax

---
+++ JavaScriptFundramentalProgram/junitTestingPrograms/vendingMachine/vendingMachineBL.js
@@ -0,0 +1,37 @@
+/**
+ * BasicPrograms/FlipCoin.
+ * @module BasicPrograms/FlipCoin
+ */
+let notes = [2000,500,200,100,50,20,10,5,1];
+let noteCounter = [];
+let len = notes.length;
+
+module.exports = {
+
+    /**
+     * it will count currency.
+     * @param {number} amount - it take amount.
+     * @return {void} .
+     */
+    countCurrency:(amount) => {
+        for(let i=0;i<len;i++){
+            if(amount >= notes[i]){
+                noteCounter[i] = Math.floor(amount/notes[i]);
+                amount = amount - (noteCounter[i] * notes[i]);
+            }
+        }
+    },
+
+    /**
+     * it will print count currency.
+     * @return {void} .
+     */
+    giveCurrency:() => {
+
+        for(let i=0;i<len;i++){
+            if(noteCounter[i] != undefined){
+                console.log(`${notes[i]} = ${noteCounter[i]}`);
+            }
+        }
+    }
+}
