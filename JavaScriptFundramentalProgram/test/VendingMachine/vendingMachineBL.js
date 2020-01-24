8d7fa4bfe02d0c227fc6fbaccd4695b3c82228c6
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-01-24 12:03:00 +0530

    [Naresh] add vendingBL for use

--- JavaScriptFundramentalProgram/test/VendingMachine/vendingMachineBL.js
+++ JavaScriptFundramentalProgram/test/VendingMachine/vendingMachineBL.js
@@ -1,11 +1,19 @@
-
-
+/**
+ * BasicPrograms/FlipCoin.
+ * @module BasicPrograms/FlipCoin
+ */
 let notes = [2000,500,200,100,50,20,10,5,1];
 let noteCounter = [];
 let len = notes.length;
 
 module.exports = {
-    countCurrency:function(amount){
+
+    /**
+     * it will count currency.
+     * @param {number} amount - it take amount.
+     * @return {void} .
+     */
+    countCurrency:(amount) => {
         for(let i=0;i<len;i++){
             if(amount >= notes[i]){
                 noteCounter[i] = Math.floor(amount/notes[i]);
@@ -14,7 +22,11 @@ module.exports = {
         }
     },
 
-    giveCurrency:function(){
+    /**
+     * it will print count currency.
+     * @return {void} .
+     */
+    giveCurrency:() => {
 
         for(let i=0;i<len;i++){
             if(noteCounter[i] != undefined){
