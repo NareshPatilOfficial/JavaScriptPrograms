fc4c1d81051ab9cf15a4d97b4cc069a357e4aeae
Shibani15 <mishrashibani384@gmail.com>
2020-02-06 19:06:10 +0530

    [Naresh] Added. main logic for Clinique Management Program

---
+++ JavaScriptProgram/objectOrientedPrograms/cliniqueManagementProgram/cliniqueManagementProgramMain.js
@@ -0,0 +1,10 @@
+const CMPlib = require("./cliniqueManagementProgramBL");
+try{
+    let main = () => {
+        let cliObj = new CMPlib.CliniqueManagment();
+        
+    }
+    main();
+}catch(err){
+    throw err;
+}
