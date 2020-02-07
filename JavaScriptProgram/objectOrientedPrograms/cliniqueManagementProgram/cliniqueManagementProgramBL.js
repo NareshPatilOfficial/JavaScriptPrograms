7bddac5fc65ba63bea26e0f75a32b31379e0e8f1
Shibani15 <mishrashibani384@gmail.com>
2020-02-07 11:29:36 +0530

    [Naresh] Refactor. change business logic and add comments

--- JavaScriptProgram/objectOrientedPrograms/cliniqueManagementProgram/cliniqueManagementProgramBL.js
+++ JavaScriptProgram/objectOrientedPrograms/cliniqueManagementProgram/cliniqueManagementProgramBL.js
@@ -1,11 +1,22 @@
+/**
+ * objectOrientedPrograms/cliniqueManagementProgram.
+ * @module objectOrientedPrograms/cliniqueManagementProgram
+ */
 const fs = require('fs');
+/**
+ * @class Doctor
+*/
 class Doctor{
-    constructor(id,name,specialization){
+    constructor(id,name,specialization,availability){
         this.id = id;
         this.name = name;
         this.specialization = specialization;
+        this.availability = availability;
     }
 }
+/**
+ * @class Patient
+*/
 class Patient{
     constructor(id,name,phone){
         this.id = id;
@@ -13,16 +24,47 @@ class Patient{
         this.phone = phone;
     }
 }
+/**
+ * @class CliniqueManagment
+*/
 class CliniqueManagment{
     constructor(){
         this.clinequeData = JSON.parse(fs.readFileSync('cliniqueData.json'));
-        if(!this.clinequeData.hasOwnProperty("doctores")){
-            this.clinequeData = Object.assign({"doctores": []},this.clinequeData);
-            if(!this.clinequeData.hasOwnProperty("patients")){
-                this.clinequeData = Object.assign({"patients": []},this.clinequeData);
+    }
+    /**
+     * @module - this method will search the doctor there in clinique json file using search parameter.
+     * @param {*} search - keyword to search for entries in json file. 
+    */
+    searchDoctor(search){
+        // console.log(this.clinequeData.doctors.length);
+        for (let i = 0; i < this.clinequeData.doctors.length; i++) {
+            if (this.clinequeData.doctors[i].name == search) {
+                console.log(this.clinequeData.doctors[i]);
+            }
+            if (this.clinequeData.doctors[i].id == search) {
+                console.log(this.clinequeData.doctors[i]);
+            }
+            if (this.clinequeData.doctors[i].specialization == search) {
+                console.log(this.clinequeData.doctors[i]);
             }
         }
-        fs.writeFileSync('cliniqueData.json',JSON.stringify(this.clinequeData));
+    }
+    /**
+     * @module - this method will search the patients there in clinique json file using search parameter.
+     * @param {*} search - keyword to search for entries in json file. 
+     */
+    searchPatient(search){
+        for (let i = 0; i < this.clinequeData.patients.length; i++) {
+            if (this.clinequeData.patients[i].name == search) {
+                console.log(this.clinequeData.patients[i]);
+            }
+            if (this.clinequeData.patients[i].id == search) {
+                console.log(this.clinequeData.patients[i]);
+            }
+            if (this.clinequeData.patients[i].phone == search) {
+                console.log(this.clinequeData.patients[i]);
+            }
+        }
     }
 }
 module.exports = {
